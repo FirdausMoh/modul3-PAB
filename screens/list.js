5f4fd1853934f66220598c7519c2517c3b30d560
Fahri Firdaus <mohamadfahri302@gmail.com>
2023-10-17 21:59:32 +0700

    first commit

---
+++ screens/list.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import {
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  Modal,
+  Button,
+  StyleSheet,
+} from 'react-native';
+
+// Dummy Data (Array of Objects)
+const datas = [
+  {
+    id: 1,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/Salinan-IMG_0823-768x512.jpg",
+  },
+  {
+    id: 2,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghjk-768x492.jpg",
+  },
+  {
+    id: 3,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghkl-768x472.jpg",
+  },
+  {
+    id: 4,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 5,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+  {
+    id: 6,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/Salinan-IMG_0823-768x512.jpg",
+  },
+  {
+    id: 7,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghjk-768x492.jpg",
+  },
+  {
+    id: 8,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghkl-768x472.jpg",
+  },
+  {
+    id: 9,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 10,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+
+];
+
+const List = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.view} onPress={() => openModal(item)}>
+        <View>
+          <Image source={{ uri: item.image }} style={styles.image} />
+          <Text style={styles.text}>{item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View>
+      <FlatList data={datas} renderItem={renderItem} keyExtractor={(item) => item.id} />
+
+      <Modal visible={modalVisible} animationType="fade">
+        <View style={styles.modalContainer}>
+        <Text style={styles.modalTitle}>{selectedItem ? selectedItem.title : ''}</Text>       
+          <Button title="Tutup" onPress={closeModal} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+// Styles
+const styles = StyleSheet.create({
+  view: {
+    padding: 15,
+    borderBottomColor: '#dddddd',
+    borderBottomWidth: 1,
+  },
+  image: {
+    height: 200,
+    width: null,
+  },
+  text: {
+    fontSize: 18,
+    paddingTop: 10,
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  modalTitle: {
+    textAlign: 'center',
+    fontSize: 24,
+    marginBottom: 30,
+  },
+  modalImage: {
+    height: 200,
+    width: null,
+  },
+});
+
+export default List;
