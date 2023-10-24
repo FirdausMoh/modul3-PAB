c069de4a4d87ad3e2277e72f5409038f07f9bf70
Fahri Firdaus <mohamadfahri302@gmail.com>
2023-10-25 02:06:12 +0700

    recode native base

--- components/separator.js
+++ components/separator.js
@@ -1,11 +1,18 @@
-import { View } from "react-native";
+// import { View } from "react-native";
 
-// Functional Component with props
+// // Functional Component with props
+// const Separator = (props) => {
+//   return <View style={{ height: props.height }}></View>;
+// };
+
+// export default Separator;
+
+import React from "react";
+import { Box } from "native-base";
+
 const Separator = (props) => {
-  return <View style={{ height: props.height }}></View>;
+  return <Box h={props.height}></Box>
 };
 
 export default Separator;
-
-
 
